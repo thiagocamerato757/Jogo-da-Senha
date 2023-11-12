84953eb3f31baedab67fa2cca762db32d42b6e84
Ana Luiza Marques <113355947+anapmarques@users.noreply.github.com>
2023-11-12 19:57:38 -0300

    botão de ir para partida

--- PROJECT/scripts/menu.py
+++ PROJECT/scripts/menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import ctypes
+from botao import iniciar_jogo
 #TODO: implementar botoes de ir para a partida e de ir para o tutorial 
 def criar_menu():
     menu = tk.Tk()
@@ -14,5 +15,10 @@ def criar_menu():
     menu_text = tk.Label(menu, text="Jogo da Senha", font=("Times New Roman", 60), fg="#00FFFF", bg="#000315")
     menu_text.pack(pady=(20, 0))  # Use pady para definir o espaço acima do widget
 
+    iniciar_jogo_button = tk.Button(menu, bg = "#000315", fg = "white", text="Iniciar Jogo", command=iniciar_jogo, width=15, font=("Times New Roman", 20))
+    iniciar_jogo_button.pack(pady=30)
+
     menu.mainloop()
     return menu
+
+criar_menu()
