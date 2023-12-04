5832c591164fff0a2cdadbaac4644fc51cdd2ea2
Ana Luiza Marques <113355947+anapmarques@users.noreply.github.com>
2023-12-03 21:53:27 -0300

    mudanca de cor

--- PROJECT/scripts/tutorial.py
+++ PROJECT/scripts/tutorial.py
@@ -10,19 +10,19 @@ def criar_tutorial(pular_callback=None):
     tutorial.iconbitmap("PROJECT/icon/window_icon.ico")
     tutorial.resizable(True, True)
     tutorial.geometry("750x500")
-    tutorial.configure(background="#000315")
+    tutorial.configure(background="#7D5B8C")
     
     # Abra o arquivo com codificação UTF-8
     with open("PROJECT\\arqs\\tutorial.txt", "r", encoding="utf-8") as arquivo:
         conteudo = arquivo.read()
 
     # Crie um widget Text para exibir o conteúdo do tutorial
-    texto_tutorial = tk.Text(tutorial, wrap="word", font=("Times New Roman", 18), fg="#FFFFFF", bg="#000315", bd=0)
+    texto_tutorial = tk.Text(tutorial, wrap="word", font=("Times New Roman", 18), fg="black", bg="#7D5B8C", bd=0)
     texto_tutorial.insert("1.0", conteudo)
     texto_tutorial.pack(fill="both", expand=True)
 
     # Crie um botão "Pular Tutorial" para fechar a janela
-    botao_pular = tk.Button(tutorial, text="Sair do Tutorial", command=pular_tutorial, height=2, width=20)
+    botao_pular = tk.Button(tutorial, text="Sair do Tutorial", bg="#B19CD9", command=pular_tutorial, height=2, width=20)
     botao_pular.pack(pady=50)
 
     tutorial.mainloop()
